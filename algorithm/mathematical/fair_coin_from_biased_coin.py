85b9e41a3018696099cbc13900f299ce7954736b
konglk <konglk@aliyun.com>
2019-07-19 11:47:05 +0800

    make a fair coin from a biased coin

---
+++ algorithm/mathematical/fair_coin_from_biased_coin.py
@@ -0,0 +1,49 @@
+"""
+Make a fair coin from a biased coin.
+We know foo() returns 0 with 60% probability. How can we ensure that 0 and 1 are returned with 50% probability?
+If we somehow get two cases with equal probability, then we are done. We call foo two times. Both calls will return 0 with
+60% probability. So the two pairs (0,1) and (1,0）will be generated with equal probability from two calls of foo. Let us
+see how.
+(0,1): The probability to get 0 followed by 1 from two calls of foo() = 0.6*0.4=0.24
+(1,0): The probability to get 1 followed by 0 from two calls of foo() = 0.4*0.6=0.24
+So the two cases appear with equal probability. The idea is to return consider only the above two cases, return 0 in one
+case, return 1 in other case. For other cases, recur until you end up in any of the above two cases.
+"""
+import random
+
+
+def fair_coin():
+    first = foo()
+    second = foo()
+    if first + second == 1:
+        return first == 0
+    return fair_coin()
+
+
+def foo():
+    r = random.random()
+    if r < 0.4:
+        return 1
+    else:
+        return 0
+
+
+sample = 10000
+trueCount = 0
+falseCount = 0
+for i in range(sample):
+    v = fair_coin()
+    if v:
+        trueCount += 1
+    elif not v:
+        falseCount += 1
+print(trueCount, falseCount, trueCount / falseCount)
+
+zeroCount = 0
+oneCount = 1
+for i in range(sample):
+    if foo() == 0:
+        zeroCount += 1
+    else:
+        oneCount += 1
+print(zeroCount, oneCount, zeroCount / oneCount)

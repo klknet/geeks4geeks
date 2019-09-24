b978a69a088fc142cc496072c590b9784d85aa0c
konglk <konglk@aliyun.com>
2019-09-24 14:42:16 +0800

    breadth first search

---
+++ algorithm/graph/breadth_first_search.py
@@ -0,0 +1,37 @@
+"""
+Breadth first search or a BFS for a graph.
+"""
+from collections import defaultdict
+
+
+def bfs(graph, start):
+    visited = [0] * graph.vertex
+    queue = [start]
+    visited[start] = 1
+    while len(queue) > 0:
+        v = queue.pop()
+        print(v, end=' ')
+        edges = graph.edge[v]
+        for e in edges:
+            if not visited[e]:
+                queue.insert(0, e)
+                visited[e] = 1
+
+
+class Graph(object):
+    def __init__(self, v):
+        self.vertex = v
+        self.edge = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.edge[u].append(v)
+
+
+graph = Graph(5)
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 2)
+graph.add_edge(2, 0)
+graph.add_edge(2, 3)
+graph.add_edge(3, 3)
+bfs(graph, 2)

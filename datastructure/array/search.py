c334c9510ef56f02e22d1374860cca14b6771ff9
konglingkai <konglk@aliyun.com>
2020-03-13 16:24:58 +0800

    find common elements in three sorted array

--- datastructure/array/search.py
+++ datastructure/array/search.py
@@ -46,6 +46,29 @@ def search_in_step_arr(arr, step, x):
     return -1
 
 
+def find_common_ele(a1, a2, a3):
+    """
+    Find common elements in three common sorted arrays.
+    :param a1:
+    :param a2:
+    :param a3:
+    :return:
+    """
+    i = j = k = 0
+    while i < len(a1) and j < len(a2) and k < len(a3):
+        if a1[i] == a2[j] == a3[k]:
+            print(a1[i], end=' ')
+            i += 1
+            j += 1
+            k += 1
+        elif a1[i] < a2[j]:
+            i += 1
+        elif a2[j] < a3[k]:
+            j += 1
+        else:
+            k += 1
+
+
 if __name__ == '__main__':
     arr = [1, 4, 45, 6, 10, -8]
     x = 16
@@ -53,3 +76,7 @@ if __name__ == '__main__':
     print("have pair of sum equals to x", pair_sum_as_x_hash(arr, x))
     arr = [2, 4, 5, 7, 7, 6]
     print("element at index", search_in_step_arr(arr, 2, 7))
+    a1 = [1, 5, 10, 20, 40, 80]
+    a2 = [6, 7, 20, 80, 100]
+    a3 = [3, 4, 15, 20, 30, 70, 80, 120]
+    find_common_ele(a1, a2, a3)

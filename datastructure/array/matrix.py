c9127604eacc7ae7adb53ed18f0531bcbe61d2c8
konglingkai <konglk@aliyun.com>
2020-04-03 11:21:08 +0800

    rotate 90 degree in anti-clockwise

--- datastructure/array/matrix.py
+++ datastructure/array/matrix.py
@@ -32,6 +32,7 @@ def rotate_matrix(m, n, matrix):
         for i in range(m - 1, row - 1, -1):
             matrix[i][col], prev = prev, matrix[i][col]
         col += 1
+    print_matrix(matrix)
 
 
 def print_matrix(matrix):
@@ -39,9 +40,33 @@ def print_matrix(matrix):
         for j in range(len(matrix[0])):
             print(matrix[i][j], end='  ')
         print()
+    print()
+
+
+def anti_clockwise(matrix):
+    """
+    rotate a matrix 90 degrees in anti-clockwise
+    :param matrix:
+    :return:
+    """
+    n = len(matrix)
+    # have n/2 cycles.
+    for i in range(n // 2):
+        for j in range(i, n - i -1):
+            tmp = matrix[i][j]
+            # move right to top
+            matrix[i][j] = matrix[j][n - i - 1]
+            # move bottom to right
+            matrix[j][n-i-1] = matrix[n-i-1][n-j-1]
+            # move left to bottom
+            matrix[n-i-1][n-j-1] = matrix[n-j-1][i]
+            # move top to left
+            matrix[n-j-1][i] = tmp
+    print_matrix(matrix)
 
 
 if __name__ == '__main__':
     matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     rotate_matrix(len(matrix), len(matrix[0]), matrix)
-    print_matrix(matrix)
+    matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+    anti_clockwise(matrix)

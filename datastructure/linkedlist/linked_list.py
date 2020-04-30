a219c06f66e5cec634ce735cc04b05c32ef3bea0
konglingkai <konglk@aliyun.com>
2020-04-30 18:02:08 +0800

    find length of loop in linked list.

--- datastructure/linkedlist/linked_list.py
+++ datastructure/linkedlist/linked_list.py
@@ -4,9 +4,9 @@ Linked list.
 
 
 class _Node:
-    def __init__(self, data):
+    def __init__(self, data, n=None):
         self.data = data
-        self.next = None
+        self.next = n
 
 
 class LinedList(object):
@@ -14,17 +14,35 @@ class LinedList(object):
         self.head = None
         self.num = 0
 
-    def add_node(self, data):
-        node = _Node(data)
-        node.next = self.head
-        self.head = node
+    def append(self, data):
+        """
+        Append a element at end of list.
+        :param data:
+        :return:
+        """
+        if self.empty():
+            self.head = _Node(data)
+        else:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = _Node(data)
         self.num += 1
 
     def add_all(self, arr):
+        """
+        Add array elements to list.
+        :param arr:
+        :return:
+        """
         for i in range(len(arr) - 1, -1, -1):
-            self.add_node(arr[i])
+            self.push(arr[i])
 
     def remove_all(self):
+        """
+        Remove all elements in list.
+        :return:
+        """
         curr = self.head
         self.head = None
         while curr is not None:
@@ -34,12 +52,22 @@ class LinedList(object):
         self.num = 0
 
     def push(self, data):
+        """
+        Push element at head.
+        :param data:
+        :return:
+        """
         node = _Node(data)
         node.next = self.head
         self.head = node
         self.num += 1
 
     def pop(self, idx=None):
+        """
+        Pop the Nth element.
+        :param idx:
+        :return:
+        """
         if self.num == 0:
             raise Exception("empty list")
         if idx is None:
@@ -63,6 +91,11 @@ class LinedList(object):
         return res
 
     def exist(self, x):
+        """
+        Find if a element exist in list.
+        :param x:
+        :return:
+        """
         cur = self.head
         while cur is not None:
             if cur.data == x:
@@ -71,12 +104,22 @@ class LinedList(object):
         return False
 
     def get_from_end(self, idx):
+        """
+        Get the Nth element from end.
+        :param idx:
+        :return:
+        """
         if idx >= self.num:
             raise Exception("Out of Index Boundary Exception")
-        idx = self.num-idx
+        idx = self.num - idx
         return self.get(idx)
 
     def get(self, idx):
+        """
+        Get the Nth element.
+        :param idx:
+        :return:
+        """
         cur = self.head
         for i in range(idx):
             if cur:
@@ -88,6 +131,10 @@ class LinedList(object):
         return None
 
     def get_mid(self):
+        """
+        Get the middle element in list.
+        :return:
+        """
         if self.empty():
             return None
         fast = slow = self.head
@@ -96,7 +143,29 @@ class LinedList(object):
             slow = slow.next
         return slow.data
 
+    def count(self, data):
+        """
+        Count the number of occurrences a given int in linked list.
+        :param data:
+        :return:
+        """
+        if self.empty():
+            return 0
+        return self._occurrence(self.head, data)
+
+    def _occurrence(self, node, data):
+        if not node:
+            return 0
+        c = self._occurrence(node.next, data)
+        if node.data == data:
+            c += 1
+        return c
+
     def traverse(self):
+        """
+        Print all elements.
+        :return:
+        """
         cur = self.head
         while cur is not None:
             print(cur.data, end=' ')
@@ -115,13 +184,13 @@ if __name__ == '__main__':
     l.add_all([0, 1, 2, 3, 4, 5, 6])
     print(l.exist(1))
     print(l.exist(10))
-    # print(l.get(2))
-    # print(l.get_from_end(2))
+    print(l.get(2))
+    print(l.get_from_end(2))
     print(l.get_mid())
-    l.push(7)
+    l.append(7)
     print(l.get_mid())
     l.traverse()
-    # print(l.pop(2))
-    # print(l.pop())
-    # l.traverse()
-    # l.remove_all()
+    print(l.pop(2))
+    print(l.pop())
+    l.traverse()
+    l.remove_all()
